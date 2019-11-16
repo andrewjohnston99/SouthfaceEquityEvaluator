48b2ed2ea8b597a1bb3b827961280699a433f14a
Julian Stephens <julianstephens55@gmail.com>
2019-11-16 17:32:35 -0500

    Complete dashboard page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,80 @@
-@extends('layouts.app')
+@extends('common.default')
+
+@section('title', 'Home')
+
+@section('css')
+    <style>
+        html, body {
+            background-color: #FFFFFE;
+        }
+    </style>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="dashboard container">
+        <div class="row d-flex justify-content-between align-items-center header">
+            <a href="{{ url('landing') }}">
+                <img src="{{ URL::asset('images/southface-logo-sm.png') }}" />
+            </a>
+            <button id="settingsBtn" class="settings-btn d-flex justify-content-center align-items-center" type="button">
+                <span>
+                    @include('icons.settings')
+                </span>
+                Settings
+            </button>
+        </div>
+        <div class="row d-flex hero">
+            <div class="col">
+                <img src="{{ URL::asset('images/equity.png') }}" />
+            </div>
+            <div class="col d-flex align-items-center">
+                <h1 class="equity">Equity Evaluator</h1>
+            </div>
+            <div class="col flex-grow-1 d-flex align-items-center justify-content-end">
+                <button id="addBtn" class="add-btn d-flex justify-content-center align-items-center" type="button">
+                    New Project
+                    <span>
+                        @include('icons.add')
+                    </span>
+                </button>
+            </div>
+        </div>
+        <div class="row projects d-flex justify-content-between">
+            <div class="container project-card">
+                <div class="row project-title">
+                    <h3>Community Name</h3>
+                </div>
+                <div class="row">
+                    <p>Review Date</p>
+                </div>
+                <div class="row">
+                    <p>Site Address</p>
+                </div>
+                <div class="row">
+                    <p>Charrette Date</p>
+                </div>
+                <div class="row">
+                    <p>Kickoff Date</p>
+                </div>
+            </div>
+            <div class="container project-card">
+                <div class="row project-title">
+                    <h3>Community Name</h3>
+                </div>
+                <div class="row">
+                    <p>Review Date</p>
+                </div>
+                <div class="row">
+                    <p>Site Address</p>
+                </div>
+                <div class="row">
+                    <p>Charrette Date</p>
+                </div>
+                <div class="row">
+                    <p>Kickoff Date</p>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
 @endsection
