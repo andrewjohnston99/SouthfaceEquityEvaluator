1c2b7067931efd1837f59c32fd85a218855d40d4
Julian Stephens <julianstephens55@gmail.com>
2020-02-07 16:05:33 -0500

    Clean up project template

---
+++ resources/views/tables/gen-equity.blade.php
@@ -0,0 +1,79 @@
+<div id="genEquity" class="table-responsive">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <td class="text-center align-middle font-weight-bold" colspan="2">GENERAL EQUITY</td>
+                <td>Points</td>
+                <td>Planned</td>
+                <td>Achieved</td>
+                <td>Confirmation Documents</td>
+                <td>Notes: DATE</td>
+            </tr>
+            <tr class="table-info">
+                <th scope="col" colspan="7">OPTIONAL</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">GE 1.0</th>
+                <td>Specialized Housing (Senior, Assisted Living, Transitional)</td>
+                <td>20</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">GE 1.1</th>
+                <td>Lifecycle Housing (raising children through aging in place)</td>
+                <td>15</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">GE 1.2</th>
+                <td>Interim uses</td>
+                <td>10</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">GE 1.3</th>
+                <td>Small scale development that fosters hyper local economic development.</td>
+                <td>15</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">GE 1.4</th>
+                <td>Permanent Housing</td>
+                <td>20</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">GE 1.5</th>
+                <td>Car sharing program (e.g. ZipCar)</td>
+                <td>5+</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr class="table-secondary">
+                <td colspan="3">GENERAL EQUITY TOTAL</td>
+                <td></td>
+                <td></td>
+                <td colspan="2"></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
