304d6d21525bb36e06e4de98b4afb5e0a8b7bf38
Julian Stephens <julianstephens55@gmail.com>
2020-02-11 13:02:58 -0500

    Fix table text align

--- resources/views/tables/gen-equity.blade.php
+++ resources/views/tables/gen-equity.blade.php
@@ -1,67 +1,67 @@
 <div id="genEquity" class="table-responsive">
-    <table class="table table-bordered">
+    <table id="equityTable" class="table table-bordered">
         <thead>
-            <tr>
-                <td class="text-center align-middle font-weight-bold" colspan="2">GENERAL EQUITY</td>
-                <td>Points</td>
-                <td>Planned</td>
-                <td>Achieved</td>
-                <td>Confirmation Documents</td>
-                <td>Notes: DATE</td>
-            </tr>
-            <tr class="table-info">
-                <th scope="col" colspan="7">OPTIONAL</th>
+            <tr class="text-center">
+                <th class="text-center align-middle font-weight-bold" colspan="2">GENERAL EQUITY</th>
+                <th>Points</th>
+                <th>Planned</th>
+                <th>Achieved</th>
+                <th>Confirmation Documents</th>
+                <th>Notes: DATE</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
+            <tr class="table-info">
+                <td class="font-style-bold" scope="col" colspan="7">OPTIONAL</td>
+            </tr>
+            <tr class="text-center">
                 <th scope="row">GE 1.0</th>
-                <td>Specialized Housing (Senior, Assisted Living, Transitional)</td>
+                <td class="text-left">Specialized Housing (Senior, Assisted Living, Transitional)</td>
                 <td>20</td>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
             </tr>
-            <tr>
+            <tr class="text-center">
                 <th scope="row">GE 1.1</th>
-                <td>Lifecycle Housing (raising children through aging in place)</td>
+                <td class="text-left">Lifecycle Housing (raising children through aging in place)</td>
                 <td>15</td>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
             </tr>
-            <tr>
+            <tr class="text-center">
                 <th scope="row">GE 1.2</th>
-                <td>Interim uses</td>
+                <td class="text-left">Interim uses</td>
                 <td>10</td>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
             </tr>
-            <tr>
+            <tr class="text-center">
                 <th scope="row">GE 1.3</th>
-                <td>Small scale development that fosters hyper local economic development.</td>
+                <td class="text-left">Small scale development that fosters hyper local economic development.</td>
                 <td>15</td>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
             </tr>
-            <tr>
+            <tr class="text-center">
                 <th scope="row">GE 1.4</th>
-                <td>Permanent Housing</td>
+                <td class="text-left">Permanent Housing</td>
                 <td>20</td>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
             </tr>
-            <tr>
+            <tr class="text-center">
                 <th scope="row">GE 1.5</th>
-                <td>Car sharing program (e.g. ZipCar)</td>
+                <td class="text-left">Car sharing program (e.g. ZipCar)</td>
                 <td>5+</td>
                 <td></td>
                 <td></td>
