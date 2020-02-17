3860edb7710abcf4839981ac12455ec7b541c426
Andrew Johnston <42980380+andrewjohnston99@users.noreply.github.com>
2020-02-17 13:39:07 -0500

    Upgrade symfony to v4.4.3 to fix vulnerability

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -31,6 +31,7 @@ return array(
     'Symfony\\Component\\HttpFoundation\\' => array($vendorDir . '/symfony/http-foundation'),
     'Symfony\\Component\\Finder\\' => array($vendorDir . '/symfony/finder'),
     'Symfony\\Component\\EventDispatcher\\' => array($vendorDir . '/symfony/event-dispatcher'),
+    'Symfony\\Component\\ErrorHandler\\' => array($vendorDir . '/symfony/error-handler'),
     'Symfony\\Component\\Debug\\' => array($vendorDir . '/symfony/debug'),
     'Symfony\\Component\\CssSelector\\' => array($vendorDir . '/symfony/css-selector'),
     'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
