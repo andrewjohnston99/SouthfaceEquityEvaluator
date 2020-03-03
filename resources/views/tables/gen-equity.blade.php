29648135622cf8dea9115d45f27734070aaef82f
Julian Stephens <julianstephens55@gmail.com>
2020-03-03 10:29:08 -0500

    Add pretty-checkbox to gen equity table

--- resources/views/tables/gen-equity.blade.php
+++ resources/views/tables/gen-equity.blade.php
@@ -17,103 +17,163 @@
             <tr class="text-center">
                 <th scope="row">GE 1.0</th>
                 <td class="text-left">Specialized Housing (Senior, Assisted Living, Transitional)</td>
-                <td>20</td>
-                <td id="e10" class="editable">
-                    <input id="e10-input" class="planned" type="checkbox" data-value="20" />&nbsp;
+                <td class="align-middle">20</td>
+                <td id="e10" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="e10-input" class="planned" type="checkbox" data-value="20" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="f10" class="editable">
-                    <input id="f10-input" type="checkbox" data-value="20" />&nbsp;
+                <td id="f10" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="f10-input" type="checkbox" data-value="20" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="g10" class="editable">
-                    <input id="g10-input" type="text" />&nbsp;
+                <td id="g10" class="align-middle">
+                    <input id="g10-input" type="text" />
                 </td>
-                <td id="h10" class="editable">
-                    <input id="h10-input" type="text" />&nbsp;
+                <td id="h10" class="align-middle">
+                    <input id="h10-input" type="text" />
                 </td>
             </tr>
             <tr class="text-center">
                 <th scope="row">GE 1.1</th>
                 <td class="text-left">Lifecycle Housing (raising children through aging in place)</td>
-                <td>15</td>
-                <td id="e11" class="editable">
-                    <input id="e11-input" class="planned" type="checkbox" data-value="15" />&nbsp;
+                <td class="align-middle">15</td>
+                <td id="e11" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="e11-input" class="planned" type="checkbox" data-value="15" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="f11" class="editable">
-                    <input id="f11-input" type="checkbox" data-value="15" />&nbsp;
+                <td id="f11" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="f11-input" type="checkbox" data-value="15" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="g11" class="editable">
-                    <input id="g11-input" type="text" />&nbsp;
+                <td id="g11" class="align-middle">
+                    <input id="g11-input" type="text" />
                 </td>
-                <td id="h11" class="editable">
-                    <input id="h11-input" type="text" />&nbsp;
+                <td id="h11" class="align-middle">
+                    <input id="h11-input" type="text" />
                 </td>
             </tr>
             <tr class="text-center">
                 <th scope="row">GE 1.2</th>
                 <td class="text-left">Interim uses</td>
-                <td>10</td>
-                <td id="e12" class="editable">
-                    <input id="e12-input" class="planned" type="checkbox" data-value="10" />&nbsp;
+                <td class="align-middle">10</td>
+                <td id="e12" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="e12-input" class="planned" type="checkbox" data-value="10" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="f12" class="editable">
-                    <input id="f12-input" type="checkbox" data-value="10" />&nbsp;
+                <td id="f12" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="f12-input" type="checkbox" data-value="10" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="g12" class="editable">
-                    <input id="g12-input" type="text" />&nbsp;
+                <td id="g12" class="align-middle">
+                    <input id="g12-input" type="text" />
                 </td>
-                <td id="h12" class="editable">
-                    <input id="h12-input" type="text" />&nbsp;
+                <td id="h12" class="align-middle">
+                    <input id="h12-input" type="text" />
                 </td>
             </tr>
             <tr class="text-center">
                 <th scope="row">GE 1.3</th>
                 <td class="text-left">Small scale development that fosters hyper local economic development.</td>
-                <td>15</td>
-                <td id="e13" class="editable">
-                    <input id="e13-input" class="planned" type="checkbox" data-value="15" />&nbsp;
+                <td class="align-middle">15</td>
+                <td id="e13" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="e13-input" class="planned" type="checkbox" data-value="15" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="f13" class="editable">
-                    <input id="f13-input" type="checkbox" data-value="15" />&nbsp;
+                <td id="f13" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="f13-input" type="checkbox" data-value="15" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="g13" class="editable">
-                    <input id="g13-input" type="text" />&nbsp;
+                <td id="g13" class="align-middle">
+                    <input id="g13-input" type="text" />
                 </td>
-                <td id="h13" class="editable">
-                    <input id="h13-input" type="text" />&nbsp;
+                <td id="h13" class="align-middle">
+                    <input id="h13-input" type="text" />
                 </td>
             </tr>
             <tr class="text-center">
                 <th scope="row">GE 1.4</th>
                 <td class="text-left">Permanent Housing</td>
-                <td>20</td>
-                <td id="e14" class="editable">
-                    <input id="e14-input" class="planned" type="checkbox" data-value="20" />&nbsp;
+                <td class="align-middle">20</td>
+                <td id="e14" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="e14-input" class="planned" type="checkbox" data-value="20" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="f14" class="editable">
-                    <input id="f14-input" type="checkbox" data-value="20" />&nbsp;
+                <td id="f14" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="f14-input" type="checkbox" data-value="20" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="g14" class="editable">
-                    <input id="g14-input" type="text" />&nbsp;
+                <td id="g14" class="align-middle">
+                    <input id="g14-input" type="text" />
                 </td>
-                <td id="h14" class="editable">
-                    <input id="h14-input" type="text" />&nbsp;
+                <td id="h14" class="align-middle">
+                    <input id="h14-input" type="text" />
                 </td>
             </tr>
             <tr class="text-center">
                 <th scope="row">GE 1.5</th>
                 <td class="text-left">Car sharing program (e.g. ZipCar)</td>
-                <td>5+</td>
-                <td id="e15" class="editable">
-                    <input id="e15-input" class="planned" type="checkbox" data-value="5" />&nbsp;
+                <td class="align-middle">5+</td>
+                <td id="e15" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="e15-input" class="planned" type="checkbox" data-value="5" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="f15" class="editable">
-                    <input id="f15-input" type="checkbox" data-value="5" />&nbsp;
+                <td id="f15" class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="f15-input" type="checkbox" data-value="5" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
                 </td>
-                <td id="g15" class="editable">
-                    <input id="g15-input" type="text" />&nbsp;
+                <td id="g15" class="align-middle">
+                    <input id="g15-input" type="text" />
                 </td>
-                <td id="h15" class="editable">
-                    <input id="h15-input" type="text" />&nbsp;
+                <td id="h15" class="align-middle">
+                    <input id="h15-input" type="text" />
                 </td>
             </tr>
             <tr class="table-secondary text-center">
