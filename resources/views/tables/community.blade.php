e838c3faeb96a1926ffad32c1566700aa3a5fd7d
Steven-Butz <sbutz6@gatech.edu>
2020-03-04 12:28:18 -0500

    Fix community table formatting and update for interactivity

--- resources/views/tables/community.blade.php
+++ resources/views/tables/community.blade.php
@@ -2,7 +2,7 @@
     <table id="commTable" class="table table-bordered">
         <thead>
             <tr class="text-center">
-                <th class="text-center align-middle font-weight-bold" colspan="2">BALANCED COMMUNITY</th>
+                <th class="text-center align-middle font-weight-bold title" colspan="2">BALANCED COMMUNITY</th>
                 <th class="align-middle points">Points</th>
                 <th class="align-middle planned">Planned</th>
                 <th class="align-middle achieved">Achieved</th>
