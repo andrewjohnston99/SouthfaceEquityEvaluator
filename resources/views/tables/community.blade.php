deba5f20b38c1f721cf66eaf192cc2cdbffb0015
Steven Butz <sbutz6@gatech.edu>
2020-03-04 01:16:17 -0500

    fix community table formatting and update for interactivity

--- resources/views/tables/community.blade.php
+++ resources/views/tables/community.blade.php
@@ -16,118 +16,158 @@
             </tr>
             <tr class="text-center">
                 <th scope="row">BC 0.1</th>
-                <td class="text-left">Transitioning Parcels</td>
-                <td>-</td>
-                <td>-</td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="text-left">Transitioning Parcels</td> <!-- Do these need align-middle? Do I need to add textarea to the last two?  -->
+                <td class="align-middle">-</td>
+                <td class="align-middle">-</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
             </tr>
             <tr class="text-center">
                 <th class="table-info" colspan="7">OPTIONAL</th>
             </tr>
             <tr class="text-center">
-                <th scope="row" rowspan="5">BC 1.0</th> 
+                <th scope="row" rowspan="5">BC 1.0</th>                             <!-- Do I need align-middle or textarea here> -->
                 <td class="text-left">Housing</td>
                 <td class="text-center" colspan="3">Select All That Apply:</td>
-                <td></td>
-                <td></td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
             </tr>
             <tr class="text-center">
                 <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">1</span>ELI ≤ 30% AMI 19% of Units</td>
-                <td class="text-center">0+</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle">0+</td>                    <!-- Should this have text-center (what it had before), align-middle, or both? -->
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
             <tr class="text-center">
                 <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">2</span>VLI 31-50% AMI 10% of Units</td>
-                <td class="text-center">0+</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle">0+</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
             <tr class="text-center">
                 <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">3</span>LI 51-80% AMI 13% of Units</td>
-                <td class="text-center">0+</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle">0+</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
             <tr class="text-center">
                 <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">4</span>Workforce 81-120% AMI 12% of Units</td>
-                <td class="text-center">0+</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle">0+</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
             <tr class="text-center">
                 <th scope="row" rowspan="6">BC 1.1</th>
             </tr>
             <tr class="text-center">
                 <td class="text-left">Mixed Use - Percentage of Lot Area</td>
-                <td class="text-center" colspan="3">Select One:</td>
-                <td></td>
-                <td></td>
+                <td class="text-center" colspan="3">Select One:</td>                         <!-- align-middle or text-center? -->
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
             </tr>
             <tr class="text-center">
                 <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">A.</span>60%</td>
-                <td class="text-center">2</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle">2</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
             <tr class="text-center">
                 <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">B.</span>70%</td>
-                <td class="text-center">3</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle">3</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
             <tr class="text-center">
                 <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">C.</span>80%</td>
-                <td class="text-center">4</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle">4</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
             <tr class="text-center">
                 <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">D.</span>90%</td>
-                <td class="text-center">5</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle">5</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
             <tr class="text-center">
                 <th scope="row">BC 1.2</th> 
                 <td class="text-left">LEFT INTENTIONALLY BLANK</td>
-                <td class="text-center" colspan="1">TBD</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle" colspan="1">TBD</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
             <tr class="text-center">
                 <th scope="row">BC 1.3</th> 
                 <td class="text-left">LEFT INTENTIONALLY BLANK</td>
-                <td class="text-center" colspan="1">TBD</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="align-middle" colspan="1">TBD</td>
+                <td class="align-middle"></td>
+                <td class="align-middle"></td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
             </tr>
-            <tr class="table-secondary">
+            <tr class="table-secondary text-center">
                 <td colspan="3">BALANCED COMMUNITY TOTAL</td>
-                <td>0</td>
-                <td>0</td>
+                <td id="comm_planned">0</td>
+                <td id="comm_actual">0</td>
                 <td colspan="2"></td>
             </tr>
         </tbody>
