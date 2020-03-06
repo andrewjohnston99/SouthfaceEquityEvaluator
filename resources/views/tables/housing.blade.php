5b3f87f9a671a509e0595c01e5cef596f62e23c9
Steven-Butz <sbutz6@gatech.edu>
2020-03-06 11:18:02 -0500

    Fix housing table formatting and update for interactivity

--- resources/views/tables/housing.blade.php
+++ resources/views/tables/housing.blade.php
@@ -16,13 +16,13 @@
             </tr>
             <tr class="text-center">
                 <th scope="row" rowspan="5">HD 1.0</th> 
-                <td class="text-left">Housing</td>
+                <td class="text-left align-middle">Housing</td>
                 <td class="align-middle" colspan="3">Select All That Apply:</td>
                 <td class="align-middle"></td>
                 <td class="align-middle"></td>
             </tr>
             <tr class="text-center">
-                <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">1</span>ELI ≤ 30% AMI 19% of Units</td>
+                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">1</span>ELI ≤ 30% AMI 19% of Units</td>
                 <td class="align-middle">0+</td>
                 <td class="align-middle">
                     <div class="pretty p-default p-thick p-smooth">
@@ -48,7 +48,7 @@
                 </td>
             </tr>
             <tr class="text-center">
-                <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">2</span>VLI 31-50% AMI 10% of Units</td>
+                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">2</span>VLI 31-50% AMI 10% of Units</td>
                 <td class="align-middle">0+</td>
                 <td class="align-middle">
                     <div class="pretty p-default p-thick p-smooth">
@@ -74,7 +74,7 @@
                 </td>
             </tr>
             <tr class="text-center">
-                <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">3</span>LI 51-80% AMI 13% of Units</td>
+                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">3</span>LI 51-80% AMI 13% of Units</td>
                 <td class="align-middle">0+</td>
                 <td class="align-middle">
                     <div class="pretty p-default p-thick p-smooth">
@@ -100,7 +100,7 @@
                 </td>
             </tr>
             <tr class="text-center">
-                <td class="text-left"><span class="font-weight-bold" style="margin-right: 30px;">4</span>Workforce 81-120% AMI 12% of Units</td>
+                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">4</span>Workforce 81-120% AMI 12% of Units</td>
                 <td class="align-middle">0+</td>
                 <td class="align-middle">
                     <div class="pretty p-default p-thick p-smooth">
@@ -127,7 +127,7 @@
             </tr>
             <tr class="text-center">
                 <th scope="row">HD 1.1</th> 
-                <td class="text-left">LEFT INTENTIONALLY BLANK</td>
+                <td class="text-left align-middle">LEFT INTENTIONALLY BLANK</td>
                 <td class="align-middle" colspan="1">TBD</td>
                 <td class="align-middle">
                     <div class="pretty p-default p-thick p-smooth">
@@ -154,7 +154,7 @@
             </tr>
             <tr class="text-center">
                 <th scope="row">HD 1.2</th> 
-                <td class="text-left">LEFT INTENTIONALLY BLANK</td>
+                <td class="text-left align-middle">LEFT INTENTIONALLY BLANK</td>
                 <td class="align-middle" colspan="1">TBD</td>
                 <td class="align-middle">
                     <div class="pretty p-default p-thick p-smooth">
@@ -180,9 +180,9 @@
                 </td>
             </tr>
             <tr class="table-secondary text-center">
-                <td colspan="3">HOUSING DIVERSITY TOTAL</td>
-                <td id="house_planned">0</td>
-                <td id="house_actual">0</td>
+                <td class="align-middle" colspan="3">HOUSING DIVERSITY TOTAL</td>
+                <td class="align-middle" id="house_planned">0</td>
+                <td class="align-middle" id="house_actual">0</td>
                 <td colspan="2"></td>
             </tr>
         </tbody>
