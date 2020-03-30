d2200efbbbc90d9f3b10a1b64765045ba155e7e5
Andrew Johnston <42980380+andrewjohnston99@users.noreply.github.com>
2020-03-30 17:57:53 -0400

    Add Password Change Functionality

--- resources/views/security.blade.php
+++ resources/views/security.blade.php
@@ -1,35 +1,23 @@
-
-
-<form method="POST" action="{{ route('password.update') }}" class="mx-auto">
-    @csrf
+<form method="POST" action="{{ route('change.password') }}" class="mx-auto">
+    @csrf 
+   
+    @foreach ($errors->all() as $error)
+        <p class="text-danger">{{ $error }}</p>
+    @endforeach 
 
     <div class="form-group">
-        <label for="email">Email Address</label>
-        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-        @error('email')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
+        <label for="password">Current Password</label>
+            <input id="password" type="password" class="form-control" name="current_password" autocomplete="current-password">
     </div>
     <div class="form-group">
-        <label for="password">New Password</label>
-        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-        @error('password')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
+        <label for="password">New Passowrd</label>
+            <input id="new_password" type="password" class="form-control" name="new_password" autocomplete="current-password">
     </div>
     <div class="form-group">
-        <label for="password-confirm">Confirm New Password</label>
-        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+        <label for="password">New Confirm Password</label>
+            <input id="new_confirm_password" type="password" class="form-control" name="new_confirm_password" autocomplete="current-password">
     </div>
     <div class="text-right">
-        <button type="submit" class="btn">Change Password</button>
-    </div>
+            <button type="submit" class="btn btn-primary">Update Password</button>
+    </div>    
 </form>
-
-
-
