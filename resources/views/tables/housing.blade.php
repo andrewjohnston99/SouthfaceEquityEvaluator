42ec816ee0653f01ad102b6dc1b64f0db0e36d37
Steven-Butz <sbutz6@gatech.edu>
2020-03-30 11:27:23 -0400

    Fix formatting issues in Housing Table

--- resources/views/tables/housing.blade.php
+++ resources/views/tables/housing.blade.php
@@ -13,99 +13,85 @@
                 <th class="table-info" colspan="6">OPTIONAL</th>
             </tr>
             <tr class="text-center">
-                <th scope="row" rowspan="5">HD 1.0</th>
+                <th scope="row" rowspan="5" style="padding-top: 30px;">HD 1.0</th>
                 <td class="text-left align-middle">Housing</td>
                 <td class="align-middle" colspan="2">Select All That Apply:</td>
+                <td class="align-middle">-</td>
+            </tr>
+            <tr class="text-center">
+                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">1.</span>ELI ≤ 30% AMI 19% of Units</td>
+                <td class="align-middle">0+</td>
+                <td class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="" type="checkbox" data-value="" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">2.</span>VLI 31-50% AMI 10% of Units</td>
+                <td class="align-middle">0+</td>
+                <td class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="" type="checkbox" data-value="" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">3.</span>LI 51-80% AMI 13% of Units</td>
+                <td class="align-middle">0+</td>
+                <td class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="" type="checkbox" data-value="" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">4.</span>Workforce 81-120% AMI 12% of Units</td>
+                <td class="align-middle">0+</td>
+                <td class="align-middle">
+                    <div class="pretty p-default p-thick p-smooth">
+                        <input id="" type="checkbox" data-value="" />
+                        <div class="state p-success-o">
+                            <label></label>
+                        </div>
+                    </div>
+                </td>
+                <td class="align-middle">
+                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" style="padding-top: 30px;">HD 1.1</th>
+                <td class="text-left align-middle">LEFT INTENTIONALLY BLANK</td>
+                <td class="align-middle" colspan="1">TBD</td>
                 <td class="align-middle"></td>
-            </tr>
-            <tr class="text-center">
-                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">1</span>ELI ≤ 30% AMI 19% of Units</td>
-                <td class="align-middle">0+</td>
-                <td class="align-middle">
-                    <div class="pretty p-default p-thick p-smooth">
-                        <input id="" type="checkbox" data-value="" />
-                        <div class="state p-success-o">
-                            <label></label>
-                        </div>
-                    </div>
-                </td>
                 <td class="align-middle">
                     <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
                 </td>
             </tr>
             <tr class="text-center">
-                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">2</span>VLI 31-50% AMI 10% of Units</td>
-                <td class="align-middle">0+</td>
-                <td class="align-middle">
-                    <div class="pretty p-default p-thick p-smooth">
-                        <input id="" type="checkbox" data-value="" />
-                        <div class="state p-success-o">
-                            <label></label>
-                        </div>
-                    </div>
-                </td>
-                <td class="align-middle">
-                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
-                </td>
-            </tr>
-            <tr class="text-center">
-                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">3</span>LI 51-80% AMI 13% of Units</td>
-                <td class="align-middle">0+</td>
-                <td class="align-middle">
-                    <div class="pretty p-default p-thick p-smooth">
-                        <input id="" type="checkbox" data-value="" />
-                        <div class="state p-success-o">
-                            <label></label>
-                        </div>
-                    </div>
-                </td>
-                <td class="align-middle">
-                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
-                </td>
-            </tr>
-            <tr class="text-center">
-                <td class="text-left align-middle"><span class="font-weight-bold" style="margin-right: 30px;">4</span>Workforce 81-120% AMI 12% of Units</td>
-                <td class="align-middle">0+</td>
-                <td class="align-middle">
-                    <div class="pretty p-default p-thick p-smooth">
-                        <input id="" type="checkbox" data-value="" />
-                        <div class="state p-success-o">
-                            <label></label>
-                        </div>
-                    </div>
-                </td>
-                <td class="align-middle">
-                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
-                </td>
-            </tr>
-            <tr class="text-center">
-                <th scope="row">HD 1.1</th>
+                <th scope="row" style="padding-top: 30px;">HD 1.2</th>
                 <td class="text-left align-middle">LEFT INTENTIONALLY BLANK</td>
                 <td class="align-middle" colspan="1">TBD</td>
-                <td class="align-middle">
-                    <div class="pretty p-default p-thick p-smooth">
-                        <input id="" type="checkbox" data-value="" />
-                        <div class="state p-success-o">
-                            <label></label>
-                        </div>
-                    </div>
-                </td>
-                <td class="align-middle">
-                    <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
-                </td>
-            </tr>
-            <tr class="text-center">
-                <th scope="row">HD 1.2</th>
-                <td class="text-left align-middle">LEFT INTENTIONALLY BLANK</td>
-                <td class="align-middle" colspan="1">TBD</td>
-                <td class="align-middle">
-                    <div class="pretty p-default p-thick p-smooth">
-                        <input id="" type="checkbox" data-value="" />
-                        <div class="state p-success-o">
-                            <label></label>
-                        </div>
-                    </div>
-                </td>
+                <td class="align-middle"></td>
                 <td class="align-middle">
                     <textarea id="" class="form-control form-control-sm" placeholder="Example text..."></textarea>
                 </td>
