621fa472d40bc59d63a6c70ba564edf3a9c116a6
Steven-Butz <sbutz6@gatech.edu>
2020-04-16 12:26:12 -0400

    Create Contacts and Info table

---
+++ resources/views/tables/contact.blade.php
@@ -0,0 +1,159 @@
+<div id="community" class="table-responsive">
+    <table id="contactTable" class="table table-bordered">
+        <thead>
+            <tr class="text-center">
+                <th class="text-center align-middle font-weight-bold title table-warning" colspan="2">Contacts</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="text-center">
+                <th scope="row" class="align-middle" style="width:300px">Email:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Community Name:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Community Address:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Community GPS:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th class="table-info" colspan="2">Developer and Primary Contract</th>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Phone:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Email:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Address:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Fax:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th class="table-info" colspan="2">General Contractor Project Manager</th>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Phone:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Email:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Address:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Fax:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th class="table-info" colspan="2">GC Site Supervisor Contact</th>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Phone:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Email:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table class="table table-bordered">
+        <thead>
+            <tr class="text-center">
+                <th class="text-center align-middle font-weight-bold title table-warning" colspan="2">Project Information</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="text-center">
+                <th scope="row" class="align-middle" style="width:535px">Acerage:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Approximate amount of greenspace:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Number of residential units:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Number of multi-family units:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Number of single family units:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr class="text-center">
+                <th scope="row" class="align-middle">Square footage of commercial space:</th>
+                <td class="align-middle">
+                    <textarea id="g11-input" class="form-control form-control-sm" placeholder="Example text..."></textarea>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
