84c949c11c7dfae2dbd9cc1d27c2f769b8d8c47c
Julian Stephens <julianstephens55@gmail.com>
2020-04-22 13:46:52 -0400

    Clean up items seeder

--- database/seeds/ItemsSeeder.php
+++ database/seeds/ItemsSeeder.php
@@ -20,7 +20,6 @@ class ItemsSeeder extends Seeder
         $token = config('prismic.token');
         $api = Api::get($url, $token);
 
-        // Retrieve table document IDs
         $tables = $api->query([Predicates::at('document.type', 'projecttable')]);
         $tableIDs =[];
 
@@ -28,24 +27,33 @@ class ItemsSeeder extends Seeder
             $tableIDs[current($doc->data->table_name)->text] = $doc->id;
         }
 
-        // Retrieve general equity items
-        $dbItems = Item::where('table_id', 1)->pluck('name')->all();
+        foreach(ProjectTable::all() as $table) {
+            $this->seedItems($api, $table->abbrev, $tableIDs[$table->name]);
+        }
+    }
+
+    public function seedItems($api, $tableAbbrev, $tableDocId) {
+        $dbItems = Item::where('table_id', ProjectTable::where('abbrev', $tableAbbrev)->pluck('id')->first())->pluck('name')->all();
         $prismicItems = [];
 
         $response = $api->query([
             Predicates::at('document.type', 'item'),
-            Predicates::at('my.item.table', $tableIDs['General Equity'])],
+            Predicates::at('my.item.table', $tableDocId)],
             [ 'pageSize' => 100 ]
         );
 
         foreach ($response->results as $doc) {
-            $i = Item::where('name', $doc->data->item_name[0]->text)->orWhere( 'order', $doc->data->order)->first();
+            $i = Item::where('table_id', ProjectTable::where('abbrev', $tableAbbrev)->pluck('id')->first())->where(function($query) use ($doc) {
+                $query->where('name', $doc->data->item_name[0]->text)
+                ->orWhere( 'order', $doc->data->order);
+                })
+                ->first();
             if (!is_null($i)) {
                 $i->name = $doc->data->item_name[0]->text;
                 $i->order = $doc->data->order;
                 $i->required = $doc->data->required;
                 $i->instructions = isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null;
-                $i->table_id = ProjectTable::where('abbrev', 'equity')->pluck('id')->first();
+                $i->table_id = ProjectTable::where('abbrev', $tableAbbrev)->pluck('id')->first();
                 $i->save();
             } else {
                 $i = Item::create([
@@ -53,213 +61,22 @@ class ItemsSeeder extends Seeder
                     'order' => $doc->data->order,
                     'required' => $doc->data->required,
                     'instructions' => isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null,
-                    'table_id' => ProjectTable::where('abbrev', 'equity')->pluck('id')->first()
+                    'table_id' => ProjectTable::where('abbrev', $tableAbbrev)->pluck('id')->first()
                 ]);
             }
 
-            $i->table()->associate(ProjectTable::where('abbrev', 'equity')->pluck('id')->first());
+            $i->table()->associate(ProjectTable::where('abbrev', $tableAbbrev)->pluck('id')->first());
             array_push($prismicItems, $i->name);
         }
 
-        $deletedItems = array_diff($dbItems, $prismicItems);
+        if (!empty($dbItems)) {
+            $deletedItems = array_diff($dbItems, $prismicItems);
 
-        foreach($deletedItems as $item) {
-            Item::where('name', $item)->delete();
-        }
-
-        // Retrieve phyical form items
-        $dbItems = Item::where('table_id', 1)->pluck('name')->all();
-        $prismicItems = [];
-
-        $response = $api->query([
-            Predicates::at('document.type', 'item'),
-            Predicates::at('my.item.table', $tableIDs['Physical Form'])],
-            [ 'pageSize' => 100 ]
-        );
-
-        foreach ($response->results as $doc) {
-            $i = Item::where('name', $doc->data->item_name[0]->text)->orWhere( 'order', $doc->data->order)->first();
-            if (!is_null($i)) {
-                $i->name = $doc->data->item_name[0]->text;
-                $i->order = $doc->data->order;
-                $i->required = $doc->data->required;
-                $i->instructions = isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null;
-                $i->table_id = ProjectTable::where('abbrev', 'equity')->pluck('id')->first();
-                $i->save();
-            } else {
-                $i = Item::create([
-                    'name' => $doc->data->item_name[0]->text,
-                    'order' => $doc->data->order,
-                    'required' => $doc->data->required,
-                    'instructions' => isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null,
-                    'table_id' => ProjectTable::where('abbrev', 'equity')->pluck('id')->first()
-                ]);
+            foreach($deletedItems as $item) {
+                Item::where('name', $item)->delete();
             }
-
-            $i->table()->associate(ProjectTable::where('abbrev', 'physical')->pluck('id')->first());
-            array_push($prismicItems, $i->name);
-        }
-
-        $deletedItems = array_diff($dbItems, $prismicItems);
-
-        foreach($deletedItems as $item) {
-            Item::where('name', $item)->delete();
-        }
-
-        // Retrieve services items
-        $dbItems = Item::where('table_id', 1)->pluck('name')->all();
-        $prismicItems = [];
-
-        $response = $api->query([
-            Predicates::at('document.type', 'item'),
-            Predicates::at('my.item.table', $tableIDs['Services and Employment'])],
-            [ 'pageSize' => 100 ]
-        );
-
-        foreach ($response->results as $doc) {
-            $i = Item::where('name', $doc->data->item_name[0]->text)->orWhere( 'order', $doc->data->order)->first();
-            if (!is_null($i)) {
-                $i->name = $doc->data->item_name[0]->text;
-                $i->order = $doc->data->order;
-                $i->required = $doc->data->required;
-                $i->instructions = isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null;
-                $i->table_id = ProjectTable::where('abbrev', 'equity')->pluck('id')->first();
-                $i->save();
-            } else {
-                $i = Item::create([
-                    'name' => $doc->data->item_name[0]->text,
-                    'order' => $doc->data->order,
-                    'required' => $doc->data->required,
-                    'instructions' => isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null,
-                    'table_id' => ProjectTable::where('abbrev', 'equity')->pluck('id')->first()
-                ]);
-            }
-
-            $i->table()->associate(ProjectTable::where('abbrev', 'services')->pluck('id')->first());
-            array_push($prismicItems, $i->name);
-        }
-
-        $deletedItems = array_diff($dbItems, $prismicItems);
-
-        foreach($deletedItems as $item) {
-            Item::where('name', $item)->delete();
-        }
-
-        // Retrieve population items
-        $dbItems = Item::where('table_id', 1)->pluck('name')->all();
-        $prismicItems = [];
-
-        $response = $api->query([
-            Predicates::at('document.type', 'item'),
-            Predicates::at('my.item.table', $tableIDs['Population Preservation/Expansion'])],
-            [ 'pageSize' => 100 ]
-        );
-
-        foreach ($response->results as $doc) {
-            $i = Item::where('name', $doc->data->item_name[0]->text)->orWhere( 'order', $doc->data->order)->first();
-            if (!is_null($i)) {
-                $i->name = $doc->data->item_name[0]->text;
-                $i->order = $doc->data->order;
-                $i->required = $doc->data->required;
-                $i->instructions = isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null;
-                $i->table_id = ProjectTable::where('abbrev', 'equity')->pluck('id')->first();
-                $i->save();
-            } else {
-                $i = Item::create([
-                    'name' => $doc->data->item_name[0]->text,
-                    'order' => $doc->data->order,
-                    'required' => $doc->data->required,
-                    'instructions' => isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null,
-                    'table_id' => ProjectTable::where('abbrev', 'equity')->pluck('id')->first()
-                ]);
-            }
-
-            $i->table()->associate(ProjectTable::where('abbrev', 'population')->pluck('id')->first());
-            array_push($prismicItems, $i->name);
-        }
-
-        $deletedItems = array_diff($dbItems, $prismicItems);
-
-        foreach($deletedItems as $item) {
-            Item::where('name', $item)->delete();
-        }
-
-        // Retrieve community items
-        $dbItems = Item::where('table_id', 1)->pluck('name')->all();
-        $prismicItems = [];
-
-        $response = $api->query([
-            Predicates::at('document.type', 'item'),
-            Predicates::at('my.item.table', $tableIDs['Balanced Community'])],
-            [ 'pageSize' => 100 ]
-        );
-
-        foreach ($response->results as $doc) {
-            $i = Item::where('name', $doc->data->item_name[0]->text)->orWhere( 'order', $doc->data->order)->first();
-            if (!is_null($i)) {
-                $i->name = $doc->data->item_name[0]->text;
-                $i->order = $doc->data->order;
-                $i->required = $doc->data->required;
-                $i->instructions = isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null;
-                $i->table_id = ProjectTable::where('abbrev', 'equity')->pluck('id')->first();
-                $i->save();
-            } else {
-                $i = Item::create([
-                    'name' => $doc->data->item_name[0]->text,
-                    'order' => $doc->data->order,
-                    'required' => $doc->data->required,
-                    'instructions' => isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null,
-                    'table_id' => ProjectTable::where('abbrev', 'equity')->pluck('id')->first()
-                ]);
-            }
-
-            $i->table()->associate(ProjectTable::where('abbrev', 'community')->pluck('id')->first());
-            array_push($prismicItems, $i->name);
-        }
-
-        $deletedItems = array_diff($dbItems, $prismicItems);
-
-        foreach($deletedItems as $item) {
-            Item::where('name', $item)->delete();
-        }
-
-        // Retrieve housing items
-        $dbItems = Item::where('table_id', 1)->pluck('name')->all();
-        $prismicItems = [];
-
-        $response = $api->query([
-            Predicates::at('document.type', 'item'),
-            Predicates::at('my.item.table', $tableIDs['Housing Diversity'])],
-            [ 'pageSize' => 100 ]
-        );
-
-        foreach ($response->results as $doc) {
-            $i = Item::where('name', $doc->data->item_name[0]->text)->orWhere( 'order', $doc->data->order)->first();
-            if (!is_null($i)) {
-                $i->name = $doc->data->item_name[0]->text;
-                $i->order = $doc->data->order;
-                $i->required = $doc->data->required;
-                $i->instructions = isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null;
-                $i->table_id = ProjectTable::where('abbrev', 'equity')->pluck('id')->first();
-                $i->save();
-            } else {
-                $i = Item::create([
-                    'name' => $doc->data->item_name[0]->text,
-                    'order' => $doc->data->order,
-                    'required' => $doc->data->required,
-                    'instructions' => isset($doc->data->instruction_popover) ? RichText::asText($doc->data->instruction_popover) : null,
-                    'table_id' => ProjectTable::where('abbrev', 'equity')->pluck('id')->first()
-                ]);
-            }
-
-            $i->table()->associate(ProjectTable::where('abbrev', 'housing')->pluck('id')->first());
-            array_push($prismicItems, $i->name);
-        }
-
-        $deletedItems = array_diff($dbItems, $prismicItems);
-
-        foreach($deletedItems as $item) {
-            Item::where('name', $item)->delete();
         }
     }
 }
+
+
